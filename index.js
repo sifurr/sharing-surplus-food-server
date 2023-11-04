ce69e536e6e55a1a5453c7ddb205bc29f951e5df
Md Saifur Rahman <saifurrshishir@gmail.com>
2023-11-04 23:04:54 +0600

    Add post api for adding food in the foods collection

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+
+
 // mongodb
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ddl1jzo.mongodb.net/surplus-food-sharing?retryWrites=true&w=majority`;
@@ -20,19 +23,30 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-
-    // database and collections
-    const foodCollection = client
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      
+      // database and collections
+      const foodCollection = client
       .db("surplus-food-sharing")
       .collection("foods");
+
+
+      //middlewares
+      app.use(express.json());
+      
 
     // foods api
     app.get("/api/v1/foods", async (req, res) => {
       const result = await foodCollection.find().toArray();
       res.send(result);
     });
+
+    app.post("/api/v1/user/create-food", async (req, res)=>{
+        const food = req.body;
+        const result = await foodCollection.insertOne(food);
+        res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
